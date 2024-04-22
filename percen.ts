3678ff32c4b6ed6b5101536ad4b8984f3ac4c7a6
Farhat Naz <159726939+Farhat-Naz@users.noreply.github.com>
2024-04-23 01:55:29 +0500

    Add files via upload

---
+++ percen.ts
@@ -0,0 +1,20 @@
+//percentage calculator
+let percentage=60;
+if(percentage>=80){
+    console.log("You got A1");
+}
+else if(percentage>=70){
+    console.log("You got A");
+}
+else if(percentage>=60){
+    console.log("You got B");
+}
+else if(percentage>=50){
+    console.log("you got C");
+}
+else if(percentage>=40){
+    console.log("You got D");
+}
+else{
+    console.log("You are fail");
+}
